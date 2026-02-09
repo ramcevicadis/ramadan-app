f12214a0a743d71a7bb2058d1a01b391c422dfcb
ramcevicadis <74376334+ramcevicadis@users.noreply.github.com>
2026-02-09 19:48:00 +0100

    Enhance SplashScreen styles with background properties
    Added background size, position, and repeat styles to the splash screen.

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -5,15 +5,20 @@ const SplashScreen = () => {
     <div 
       className="splash-screen"
       style={{
-        backgroundImage: `url(${process.env.PUBLIC_URL}/images/gazilar2.png)`
+        backgroundImage: `url(${process.env.PUBLIC_URL}/images/gazilar2.png)`,
+        backgroundSize: 'cover',      // ← DODAJ OVO
+        backgroundPosition: 'center', // ← DODAJ OVO
+        backgroundRepeat: 'no-repeat' // ← DODAJ OVO
       }}
     >
       <div className="splash-sponsors">
         <img src={`${process.env.PUBLIC_URL}/images/logoi_beli.png`} alt="Sponsors" />
+        {/* ↑ ISPRAVLJENO: bilo src=` sada src={ */}
       </div>
       
       <img 
         src={`${process.env.PUBLIC_URL}/images/123.png`}
+        {/* ↑ ISPRAVLJENO: bilo src=` sada src={ */}
         alt="Ramadan Kareem" 
         className="splash-logo"
       />
