74ff4a0a6be27bd862d606c37bb74c4a679094f8
ramcevicadis <74376334+ramcevicadis@users.noreply.github.com>
2026-02-09 19:49:56 +0100

    Refactor SplashScreen component for better readability

--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -6,19 +6,20 @@ const SplashScreen = () => {
       className="splash-screen"
       style={{
         backgroundImage: `url(${process.env.PUBLIC_URL}/images/gazilar2.png)`,
-        backgroundSize: 'cover',      // ← DODAJ OVO
-        backgroundPosition: 'center', // ← DODAJ OVO
-        backgroundRepeat: 'no-repeat' // ← DODAJ OVO
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
       }}
     >
       <div className="splash-sponsors">
-        <img src={`${process.env.PUBLIC_URL}/images/logoi_beli.png`} alt="Sponsors" />
-        {/* ↑ ISPRAVLJENO: bilo src=` sada src={ */}
+        <img 
+          src={`${process.env.PUBLIC_URL}/images/logoi_beli.png`} 
+          alt="Sponsors" 
+        />
       </div>
       
       <img 
         src={`${process.env.PUBLIC_URL}/images/123.png`}
-        {/* ↑ ISPRAVLJENO: bilo src=` sada src={ */}
         alt="Ramadan Kareem" 
         className="splash-logo"
       />
